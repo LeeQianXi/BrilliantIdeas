c2dc034d785a8f6ee13e0ef13f6f6d51bf061129
LeeQianXi <qianxilee232@163.com>
2025-12-12 22:17:21 +0800

    test

--- BreaTest/CoroutineTest.cs
+++ BreaTest/CoroutineTest.cs
@@ -1,25 +1,66 @@
-﻿using AvaloniaUtility;
+﻿using System.Diagnostics;
+using AvaloniaUtility;
 
 namespace BreaTest;
 
 public class CoroutineTest : ICoroutinator
 {
     public CancellationTokenSource CoroutinatorCancelTokenSource { get; } = new();
-
+    
     [Fact]
-    public void Test1()
+    public void TestSync()
     {
-        this.StartCoroutine(SyncAction);
-        this.StartCoroutine(AsyncAction);
+        int count = 1000;
+        foreach (var i in Enumerable.Range(0, count))
+            this.StartCoroutine(SyncAction).Completed += CompleteCount;
+        while (count <= 0) ;
+        return;
+
+        void CompleteCount()
+        {
+            count--;
+        }
+    }
+    [Fact]
+    public void TestAsync()
+    {
+        int count = 1000;
+        foreach (var i in Enumerable.Range(0, count))
+            this.StartCoroutine(AsyncAction).Completed += CompleteCount;
+        while (count <= 0) ;
+        return;
+
+        void CompleteCount()
+        {
+            count--;
+        }
+    }
+    private static IEnumerator<YieldInstruction?> SyncAction()
+    {
+        var l = new List<int>();
+        foreach (var i in Enumerable.Range(0, 1000))
+        {
+            l.Add(i);
+            yield return null;
+            Debug.WriteLine("SyncAction: " + i);
+        }
+        Display(l);
     }
 
-    private static IEnumerator<YieldInstruction> SyncAction()
+    private static async IAsyncEnumerator<YieldInstruction?> AsyncAction()
     {
-        yield break;
+        var l = new List<int>();
+        foreach (var i in Enumerable.Range(0, 1000))
+        {
+            l.Add(i);
+            yield return null;
+            Debug.WriteLine("AsyncAction: " + i);
+        }
+        Display(l);
     }
 
-    private static async IAsyncEnumerator<YieldInstruction> AsyncAction()
+    private static void Display(IEnumerable<int> l)
     {
-        yield break;
+        Console.WriteLine(string.Join(", ", l));
     }
 }
