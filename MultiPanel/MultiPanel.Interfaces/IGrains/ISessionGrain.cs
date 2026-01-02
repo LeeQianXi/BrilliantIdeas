3fe98894d366d6e09e04b8642121c1b29138d1ec
LeeQianXi <qianxilee232@163.com>
2026-01-03 00:36:54 +0800

    save

--- MultiPanel/MultiPanel.Interfaces/IGrains/ISessionGrain.cs
+++ MultiPanel/MultiPanel.Interfaces/IGrains/ISessionGrain.cs
@@ -3,6 +3,14 @@ using Orleans;
 namespace MultiPanel.Interfaces.IGrains;
 
 [Alias("MultiPanel.Interfaces.IGrains.ISessionGrain")]
-public interface ISessionGrain : IGrainWithIntegerKey
+public interface ISessionGrain : IGrainWithGuidKey
 {
+    [Alias("BindToUser")]
+    Task BindToUser(int userId);
+
+    [Alias("VerifySession")]
+    Task<bool> VerifySession(string accessToken);
+
+    [Alias("IsSessionValid")]
+    Task<bool> IsSessionValid();
 }
